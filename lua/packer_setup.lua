50ab07e17dd092e96e8b97b4d22fe2b119e544b5
Jordy <jordy@alkema.co>
2022-05-29 15:54:51 +0200

    Added dashboard

--- lua/packer_setup.lua
+++ lua/packer_setup.lua
@@ -5,6 +5,11 @@ return require('packer').startup(function()
     -- Telescope
     use 'nvim-lua/plenary.nvim'
     use 'nvim-telescope/telescope.nvim'
+    
+    -- Alpha(dashboard)
+    use {
+        'goolord/alpha-nvim',
+    }
 
     -- Color scheme
     use {
