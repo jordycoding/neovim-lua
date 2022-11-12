d5b83bff006f8e828871c16897d0d2549ed1a373
Jordy Alkema <6128820+jordycoding@users.noreply.github.com>
2022-11-12 23:37:05 +0100

    Revert "Changed nix language server"
    This reverts commit 9e87f3acc5429e10e884cb05727c2b96008a3e41.

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -92,7 +92,7 @@ end
 
 -- Use a loop to conveniently call 'setup' on multiple servers and
 -- map buffer local keybindings when the language server attaches
-local servers = { 'tsserver', 'pyright', 'bashls', 'ansiblels', 'nil', 'rust_analyzer', 'jdtls' }
+local servers = { 'tsserver', 'pyright', 'bashls', 'ansiblels', 'rnix', 'rust_analyzer', 'jdtls' }
 for _, lsp in pairs(servers) do
   require('lspconfig')[lsp].setup {
     on_attach = on_attach,
