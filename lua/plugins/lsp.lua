5267a90dca91428a0ffe4c65cff2334264436ff9
Jordy Alkema <6128820+jordycoding@users.noreply.github.com>
2022-11-15 23:33:22 +0100

    Fixed format on save

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -43,7 +43,7 @@ local pid = vim.fn.getpid()
             maxwidth = 50,
             before = function(entry, vim_item)
                 return vim_item
-            end  
+            end
         })
     },
     experimental = {
@@ -81,7 +81,7 @@ local on_attach = function(client, bufnr)
   vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
   vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
   vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>f', '<cmd>lua vim.lsp.buf.format({ async = true })<CR>', opts)
-    if client.server_capabilities.document_formatting then
+    if client.server_capabilities.documentFormattingProvider then
         vim.cmd([[
         augroup LspFormatting
             autocmd! * <buffer>
