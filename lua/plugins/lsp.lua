5f3fdc63eb8a6cb8999b3811084c0db8e4bd3725
Jordy Alkema <6128820+jordycoding@users.noreply.github.com>
2023-02-12 19:53:29 +0100

    Csharp changes

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -130,6 +130,7 @@ end
 local omnisharp_bin = "/run/current-system/sw/bin/OmniSharp"
 require("lspconfig")["omnisharp"].setup({
     cmd = { omnisharp_bin, "--languageserver", "--hostPID", tostring(pid) },
+    env = { DOTNET_ROOT = os.getenv("DOTNET_ROOT") }
 })
 
 -- Vue stuff
