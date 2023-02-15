bd155502d67e9634c419ceb26c6a004abc9b4ca2
Jordy Alkema <6128820+jordycoding@users.noreply.github.com>
2023-02-15 16:23:57 +0100

    Added phpcs and phpcsfixer

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -339,33 +339,34 @@ require("lspconfig")["tsserver"].setup({
     end,
 })
 
-require'lspconfig'.lua_ls.setup {
-  settings = {
-    Lua = {
-      runtime = {
-        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-        version = 'LuaJIT',
-      },
-      diagnostics = {
-        -- Get the language server to recognize the `vim` global
-        globals = {'vim'},
-      },
-      workspace = {
-        -- Make the server aware of Neovim runtime files
-        library = vim.api.nvim_get_runtime_file("", true),
-      },
-      -- Do not send telemetry data containing a randomized but unique identifier
-      telemetry = {
-        enable = false,
-      },
+require 'lspconfig'.lua_ls.setup {
+    settings = {
+        Lua = {
+            runtime = {
+                -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+                version = 'LuaJIT',
+            },
+            diagnostics = {
+                -- Get the language server to recognize the `vim` global
+                globals = { 'vim' },
+            },
+            workspace = {
+                -- Make the server aware of Neovim runtime files
+                library = vim.api.nvim_get_runtime_file("", true),
+            },
+            -- Do not send telemetry data containing a randomized but unique identifier
+            telemetry = {
+                enable = false,
+            },
+        },
     },
-  },
 }
 
 local null_ls = require("null-ls")
 null_ls.setup({
     sources = {
         null_ls.builtins.diagnostics.eslint,
+        null_ls.builtins.diagnostics.phpcs,
         null_ls.builtins.code_actions.eslint,
         null_ls.builtins.formatting.prettier,
         null_ls.builtins.formatting.nixpkgs_fmt,
@@ -375,6 +376,7 @@ null_ls.setup({
         }),
         null_ls.builtins.formatting.google_java_format,
         null_ls.builtins.formatting.csharpier,
+        null_ls.builtins.formatting.phpcsfixer
     },
     on_attach = on_attach,
 })
