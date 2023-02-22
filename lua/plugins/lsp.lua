8521eb69abad079611b21482c30980751cab1fd2
Jordy Alkema <6128820+jordycoding@users.noreply.github.com>
2023-02-22 13:45:53 +0100

    Fixed latex

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -125,7 +125,7 @@ local servers = {
 	"docker_compose_language_service",
 	"dockerls",
 	"svelte",
-    "ltex-ls"
+    "ltex"
 }
 for _, lsp in pairs(servers) do
 	require("lspconfig")[lsp].setup({
