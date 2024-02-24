2ac89a49378148170ce7990ec6de21070a7ecd85
Jordy Alkema <6128820+jordycoding@users.noreply.github.com>
2024-02-24 23:36:21 +0100

    Hopefully fixed bufferline

--- lua/plugins/ui/bufferline.lua
+++ lua/plugins/ui/bufferline.lua
@@ -1,10 +1,3 @@
 return {
 	"akinsho/bufferline.nvim",
-	opts = {
-		options = {
-			mode = "tabs",
-			offsets = { { filetype = "NvimTree", text = "" } },
-		},
-		highlights = require("catppuccin.groups.integrations.bufferline").get(),
-	}
 }
--- lua/plugins/ui/catppuccin.lua
+++ lua/plugins/ui/catppuccin.lua
@@ -76,5 +76,13 @@ return {
 				}
 			end,
 		})
+
+		require("bufferline").setup({
+			options = {
+				mode = "tabs",
+				offsets = { { filetype = "NvimTree", text = "" } },
+			},
+			highlights = require("catppuccin.groups.integrations.bufferline").get(),
+		})
 	end,
 }
