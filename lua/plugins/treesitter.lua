d8ed42cc3da9a7965c76e429d0720093532c5955
Jordy Alkema <6128820+jordycoding@users.noreply.github.com>
2024-02-25 20:50:06 +0100

    Added some ts text objects

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -66,6 +66,8 @@ return {
 						["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
 						-- You can also use captures from other query groups like `locals.scm`
 						["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
+						["ai"] = { query = "@conditional.outer", desc = "Select outer part of a conditional" },
+						["ii"] = { query = "@conditional.innter", desc = "Select inner part of a conditional" },
 					},
 					-- You can choose the select mode (default is charwise 'v')
 					--
