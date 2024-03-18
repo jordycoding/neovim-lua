6c5d905fb341a9eab5aa72f5f6e8ba2ba37b09be
Jordy Alkema <6128820+jordycoding@users.noreply.github.com>
2024-03-18 15:10:21 +0100

    Why wont this fucking work

--- lua/lsp/conform.lua
+++ lua/lsp/conform.lua
@@ -1,10 +1,24 @@
+local slow_format_filetypes = {}
 require("conform").setup({
-	async = true,
-	format_on_save = {
-		async = true,
-		timeout_ms = 500,
-		lsp_fallback = true,
-	},
+	format_on_save = function(bufnr)
+		if slow_format_filetypes[vim.bo[bufnr].filetype] then
+			return
+		end
+		local function on_format(err)
+			if err and err:match("timeout$") then
+				slow_format_filetypes[vim.bo[bufnr].filetype] = true
+			end
+		end
+
+		return { timeout_ms = 200, lsp_fallback = true }, on_format
+	end,
+
+	format_after_save = function(bufnr)
+		if not slow_format_filetypes[vim.bo[bufnr].filetype] then
+			return
+		end
+		return { lsp_fallback = true }
+	end,
 	formatters_by_ft = {
 		lua = { "stylua" },
 		python = { "black" },
