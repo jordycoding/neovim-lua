7e2355f20db35c1199f5333b58e0274d5cfd722b
Jordy Alkema <6128820+jordycoding@users.noreply.github.com>
2024-04-21 21:37:18 +0200

    Finally working

--- lua/lsp/langs/vue.lua
+++ lua/lsp/langs/vue.lua
@@ -1,4 +1,7 @@
 local util = require("lspconfig.util")
 local lsputil = require("lsp.util")
 
-require("lspconfig").volar.setup({})
+require("lspconfig").volar.setup({
+	on_attach = util.on_attach,
+	capabilities = util.capabilities,
+})
