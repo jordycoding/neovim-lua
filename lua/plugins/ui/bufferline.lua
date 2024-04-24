6ae4c21e089294bb72dc0702e98920ba8cd2a687
Jordy Alkema <6128820+jordycoding@users.noreply.github.com>
2024-04-24 17:54:07 +0200

    I cant type

--- lua/plugins/ui/bufferline.lua
+++ lua/plugins/ui/bufferline.lua
@@ -1,15 +1,3 @@
 return {
 	"akinsho/bufferline.nvim",
-	dependencies = {
-		"catppuccin/nvim",
-	},
-	opts = {
-		options = {
-			mode = "tabs",
-			show_bufer_close_icons = false,
-			show_close_icon = false,
-			offsets = { { filetype = "NvimTree", text = "" } },
-		},
-		highlights = require("catppuccin.groups.integrations.bufferline").get(),
-	},
 }
--- lua/plugins/ui/catppuccin.lua
+++ lua/plugins/ui/catppuccin.lua
@@ -76,5 +76,14 @@ return {
 				}
 			end,
 		})
+		require("bufferline").setup({
+			options = {
+				mode = "tabs",
+				show_buffer_close_icons = false,
+				show_close_icon = false,
+				offsets = { { filetype = "NvimTree", text = "" } },
+			},
+			highlights = require("catppuccin.groups.integrations.bufferline").get(),
+		})
 	end,
 }
