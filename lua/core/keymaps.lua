76ba1108eed75cf4cdb48c15c51caa2e66f5c407
Jordy Alkema <6128820+jordycoding@users.noreply.github.com>
2024-05-07 09:18:04 +0200

    Added neotest

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -87,3 +87,8 @@ map("n", "<leader>b", ":lua require('dap').toggle_breakpoint()<CR>")
 map("n", "<leader>B", ":lua require('dap').set_breakpoint()<CR>")
 map("n", "<leader>dr", ":lua require('dap').repl.open()<CR>")
 map("n", "<leader>du", ":lua require('dapui').toggle()<CR>")
+
+-- Neotest
+map("n", "<leader>Nn", ":lua require('neotest').run.run()<CR>")
+map("n", "<leader>Nf", ":lua require('neotest').run.run(vim.fn.expand(" % "))")
+map("n", "<leader>Nd", ":lua require('neotest').run.run(vim.fn.getcwd())")
