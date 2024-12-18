e65fd1c442566f82e155052e1eafe90d6a9bc381
Jordy Alkema <6128820+jordycoding@users.noreply.github.com>
2024-12-19 00:25:50 +0100

    Added snacks

---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,14 @@
+return {
+	"folke/snacks.nvim",
+	priority = 1000,
+	lazy = false,
+	---@type snacks.Config
+	opts = {
+		bigfile = { enabled = true },
+		dashboard = { enabled = true },
+		input = { enabled = true },
+		notifier = { enabled = true },
+		quickfile = { enabled = true },
+		scroll = { enabled = true },
+	},
+}
--- lua/plugins/ui/otherui.lua
+++ lua/plugins/ui/otherui.lua
@@ -1,5 +1,4 @@
 return {
-	"stevearc/dressing.nvim",
 	"inside/vim-search-pulse",
 	{
 		"lukas-reineke/headlines.nvim",
