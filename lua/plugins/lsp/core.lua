cd8c65f8509e9e6b2af581355e9fa71e08dd3f40
Jordy Alkema <6128820+jordycoding@users.noreply.github.com>
2024-12-21 22:38:55 +0100

    Remove old cmp

--- lua/plugins/lsp/core.lua
+++ lua/plugins/lsp/core.lua
@@ -3,11 +3,6 @@ return {
 		"neovim/nvim-lspconfig",
 		event = { "BufReadPre", "BufNewFile" },
 		dependencies = {
-			"hrsh7th/nvim-cmp",
-			"hrsh7th/cmp-nvim-lsp-signature-help",
-			"onsails/lspkind-nvim",
-			"hrsh7th/cmp-nvim-lsp",
-			"hrsh7th/cmp-path",
 			{
 				"folke/neoconf.nvim",
 				config = true,
@@ -27,7 +22,6 @@ return {
 		"L3MON4D3/LuaSnip",
 		version = "v2.*",
 		dependencies = {
-			"saadparwaiz1/cmp_luasnip",
 			"rafamadriz/friendly-snippets",
 		},
 		config = function()
