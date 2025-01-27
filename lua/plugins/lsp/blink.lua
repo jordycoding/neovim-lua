4ddd6f52dae011e1aef417384f760bbe74c3fdaf
Jordy Alkema <6128820+jordycoding@users.noreply.github.com>
2025-01-27 21:35:29 +0100

    Update blink luasnip

--- lua/plugins/lsp/blink.lua
+++ lua/plugins/lsp/blink.lua
@@ -7,21 +7,10 @@ return {
 	opts = {
 		keymap = { preset = "enter" },
 		snippets = {
-			expand = function(snippet)
-				require("luasnip").lsp_expand(snippet)
-			end,
-			active = function(filter)
-				if filter and filter.direction then
-					return require("luasnip").jumpable(filter.direction)
-				end
-				return require("luasnip").in_snippet()
-			end,
-			jump = function(direction)
-				require("luasnip").jump(direction)
-			end,
+			preset = { "luasnip" },
 		},
 		sources = {
-			default = { "lsp", "path", "luasnip", "buffer", "lazydev", "copilot" },
+			default = { "lsp", "path", "snippets", "buffer", "lazydev", "copilot" },
 			cmdline = {},
 			providers = {
 				lazydev = { name = "LazyDev", module = "lazydev.integrations.blink", score_offset = 100 },
