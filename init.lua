545290d97556a8a1ed1aff9ae7541677339dfd8e
Jordy Alkema <6128820+jordycoding@users.noreply.github.com>
2025-03-18 00:35:58 +0100

    Fix legendary(fr this time)

--- init.lua
+++ init.lua
@@ -3,6 +3,5 @@ vim.g.mapleader = " "
 -- Import Lua modules
 require("core/options")
 require("lazy_setup")
-require("core/keymaps")
 require("core/colors")
 require("lsp")
--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -1,4 +1,3 @@
 require("lsp.core")
 -- require("lsp.cmp")
-require("lsp.mappings")
 require("lsp.langs")
