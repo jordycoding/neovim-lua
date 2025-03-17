f8ab6a880785e42bd6ead706011d196e5b9c14ce
Jordy Alkema <6128820+jordycoding@users.noreply.github.com>
2025-03-17 23:46:09 +0100

    Update volar config

--- lua/lsp/langs/vue.lua
+++ lua/lsp/langs/vue.lua
@@ -9,4 +9,10 @@ require("lspconfig").volar.setup({
 			tsdk = "$HOME/.npm-packages/lib/node_modules/typescript/lib",
 		},
 	},
+	on_new_config = function(new_config, new_root_dir)
+		local lib_path = vim.fs.find("node_modules/typescript/lib", { path = new_root_dir, upward = true })[1]
+		if lib_path then
+			new_config.init_options.typescript.tsdk = lib_path
+		end
+	end,
 })
