61b8a05956865dc20138daa2e5708abf84ab4ddc
Jordy Alkema <6128820+jordycoding@users.noreply.github.com>
2025-06-26 21:36:48 +0200

    Fix csharpier conform

--- lua/plugins/lsp/core.lua
+++ lua/plugins/lsp/core.lua
@@ -27,6 +27,34 @@ return {
 		---@module "conform"
 		---@type conform.setupOpts
 		opts = {
+			-- TODO: Remove when PR/Issue is closed https://github.com/stevearc/conform.nvim/issues/699
+			formatters = {
+				csharpier = function()
+					local useDotnet = not vim.fn.executable("csharpier")
+
+					local command = useDotnet and "dotnet csharpier" or "csharpier"
+
+					local version_out = vim.fn.system(command .. " --version")
+
+					vim.notify(version_out)
+
+					--NOTE: system command returns the command as the first line of the result, need to get the version number on the final line
+					local version_result = version_out[#version_out]
+					local major_version = tonumber((version_out or ""):match("^(%d+)")) or 0
+					local is_new = major_version >= 1
+
+					vim.notify(tostring(is_new))
+
+					local args = is_new and { "format", "$FILENAME" } or { "--write-stdout" }
+
+					return {
+						command = command,
+						args = args,
+						stdin = not is_new,
+						require_cwd = false,
+					}
+				end,
+			},
 			format_on_save = function(bufnr)
 				local slow_format_filetypes =
 					{ "dart", "javascript", "javascriptreact", "typescript", "typescriptreact" }
