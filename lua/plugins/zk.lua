67e648d8f4d53ad6fcbfc6347fb803677ce8adc5
Jordy Alkema <6128820+jordycoding@users.noreply.github.com>
2025-09-09 18:03:52 +0200

    Zk keybindings

--- lua/plugins/zk.lua
+++ lua/plugins/zk.lua
@@ -5,4 +5,31 @@ return {
 			picker = "snacks_picker",
 		})
 	end,
+	keys = {
+		{
+			"<leader>zcc",
+			"<cmd>ZkNew<cr>",
+			desc = "New zk note in current buffer",
+		},
+		{
+			"<leader>zcv",
+			"<cmd>vsplit<cr><cmd>ZkNew<cr>",
+			desc = "New zk note vertical split",
+		},
+		{
+			"<leader>zcs",
+			"<cmd>split<cr><cmd>ZkNew<cr>",
+			desc = "New zk note horizontal split",
+		},
+		{
+			"<leader>zfn",
+			"<cmd>ZkNotes<cr>",
+			desc = "Find zk notes",
+		},
+		{
+			"<leader>zft",
+			"<cmd>ZkTags<cr>",
+			desc = "Find zk tags",
+		},
+	},
 }
