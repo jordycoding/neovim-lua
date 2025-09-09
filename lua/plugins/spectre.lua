2558ce2c3d61ac87596f6431ba1e9d180d7731be
Jordy Alkema <6128820+jordycoding@users.noreply.github.com>
2025-09-09 23:57:03 +0200

    Spectre key descs

--- lua/plugins/spectre.lua
+++ lua/plugins/spectre.lua
@@ -2,9 +2,17 @@ return {
 	"nvim-pack/nvim-spectre",
 	dependencies = "nvim-lua/plenary.nvim",
 	keys = {
-		{ "<leader>S", '<cmd>lua require("spectre").open()<cr>' },
-		{ "<leader>sw", '<cmd>lua require("spectre").open_visual({select_word=true})<CR>' },
-		{ "<leader>sp", '<cmd>lua require("spectre").open_file_search({select_word=true})<CR>' },
+		{ "<leader>S", '<cmd>lua require("spectre").open()<cr>', desc = "Open spectre" },
+		{
+			"<leader>sw",
+			'<cmd>lua require("spectre").open_visual({select_word=true})<CR>',
+			desc = "Open spectre current word",
+		},
+		{
+			"<leader>sp",
+			'<cmd>lua require("spectre").open_file_search({select_word=true})<CR>',
+			desc = "Open spectre search current file",
+		},
 	},
 	cmd = {
 		"Spectre",
