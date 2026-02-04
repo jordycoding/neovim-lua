a986fc624f204bc7b477eb5befee45a4b800a36d
Jordy Alkema <6128820+jordycoding@users.noreply.github.com>
2026-02-04 01:13:38 +0100

    Add mini.surround

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -7,5 +7,18 @@ return {
 		require("mini.move").setup()
 		require("mini.pairs").setup()
 		require("mini.bracketed").setup()
+		require("mini.pairs").setup({
+			mappings = {
+				add = "gsa",
+				delete = "gsd",
+				find = "gsf",
+				find_left = "gsF",
+				highlight = "gsh",
+				replace = "gsr",
+
+				suffix_last = "l", -- Suffix to search with "prev" method
+				suffix_next = "n", -- Suffix to search with "next" method
+			},
+		})
 	end,
 }
