f54866f2e3a771832e971d0e2d5e484957631d2c
Jordy Alkema <6128820+jordycoding@users.noreply.github.com>
2026-02-04 01:20:39 +0100

    Fix treesitter fold

--- lua/plugins/treesitter/treesitter.lua
+++ lua/plugins/treesitter/treesitter.lua
@@ -53,6 +53,8 @@ return {
 			callback = function()
 				vim.treesitter.start()
 				vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+				vim.wo[0][0].foldexpr = "v:lua.vim.treesitter.foldexpr()"
+				vim.wo[0][0].foldmethod = "expr"
 			end,
 		})
 	end,
