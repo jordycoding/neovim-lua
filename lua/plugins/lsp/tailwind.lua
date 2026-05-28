8acd8c1321fabd7523c0999b28482b42b185d519
Jordy Alkema <6128820+jordycoding@users.noreply.github.com>
2026-05-29 00:33:03 +0200

    fix: Tailwind fold toggle keybinding

--- lua/plugins/lsp/tailwind.lua
+++ lua/plugins/lsp/tailwind.lua
@@ -11,9 +11,11 @@ return {
 		dependencies = { "nvim-treesitter/nvim-treesitter" },
 		ft = { "html", "svelte", "astro", "vue", "typescriptreact", "php", "blade" },
 		keys = {
-			"<localleader>tf",
-			"<cmd>TailwindFoldToggle<cr>",
-			desc = "Toggle tailwind class fold",
+			{
+				"<localleader>tf",
+				"<cmd>TailwindFoldToggle<cr>",
+				desc = "Toggle tailwind class fold",
+			},
 		},
 	},
 }
