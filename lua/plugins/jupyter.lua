b22e320636097f1ddf155a2528822ab127ec6d1b
Jordy Alkema <6128820+jordycoding@users.noreply.github.com>
2026-09-18 09:57:46 +0200

    chore: Kitty nvim jupy

--- lua/plugins/jupyter.lua
+++ lua/plugins/jupyter.lua
@@ -7,7 +7,7 @@ return {
 	config = function()
 		require("jupynvim").setup({
 			log_level = "info",
-			image_renderer = "placeholder", -- "placeholder", "kitty", or "chafa"
+			image_renderer = "kitty", -- "placeholder", "kitty", or "chafa"
 		})
 	end,
 }
