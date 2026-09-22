2f0466525b7c2797c0fd9d33f923a03328c78c8e
Jordy Alkema <6128820+jordycoding@users.noreply.github.com>
2026-09-22 20:39:48 +0200

    fix: lualine codecompanion

--- lua/plugins/ui/lualine.lua
+++ lua/plugins/ui/lualine.lua
@@ -1,62 +1,66 @@
 return {
 	"nvim-lualine/lualine.nvim",
 	event = "VeryLazy",
-	opts = {
-		options = {
-			theme = "catppuccin-nvim",
-			section_separators = { left = "", right = "" },
-			component_separators = { left = "", right = "" },
-		},
-		sections = {
-			lualine_a = { { "mode", color = { gui = "italic,bold" } } },
-			lualine_b = {
-				{ "branch", color = { gui = "italic,bold" } },
-				{ "b:gitsigns_status", color = { gui = "italic,bold" } },
-				{ "diagnostics", color = { gui = "italic,bold" } },
-			},
-			lualine_c = {
-				"%=", -- make the indicator center
-				separator = nil,
-				{
-					"harpoon2",
-					indicators = { "a", "s", "d", "f" },
-					active_indicators = { "[a]", "[s]", "[d]", "[f]" },
-					_separator = " ",
-				},
-			},
-			lualine_x = {
-				{
-					"fileformat",
-					color = { gui = "italic,bold" },
-				},
-				{
-					"encoding",
-					color = { gui = "italic,bold" },
-				},
-			},
-			lualine_y = {
-				{
+	opts = function()
+		local codecompanion = require("plugins.codecompanion.utils.lualine")
 
-					"location",
-					color = { gui = "italic,bold" },
+		return {
+			options = {
+				theme = "catppuccin-nvim",
+				section_separators = { left = "", right = "" },
+				component_separators = { left = "", right = "" },
+			},
+			sections = {
+				lualine_a = { { "mode", color = { gui = "italic,bold" } } },
+				lualine_b = {
+					{ "branch", color = { gui = "italic,bold" } },
+					{ "b:gitsigns_status", color = { gui = "italic,bold" } },
+					{ "diagnostics", color = { gui = "italic,bold" } },
 				},
-				{
-					"progress",
-					color = { gui = "italic,bold" },
+				lualine_c = {
+					"%=", -- make the indicator center
+					separator = nil,
+					{
+						"harpoon2",
+						indicators = { "a", "s", "d", "f" },
+						active_indicators = { "[a]", "[s]", "[d]", "[f]" },
+						_separator = " ",
+					},
+				},
+				lualine_x = {
+					{
+						"fileformat",
+						color = { gui = "italic,bold" },
+					},
+					{
+						"encoding",
+						color = { gui = "italic,bold" },
+					},
+				},
+				lualine_y = {
+					{
+
+						"location",
+						color = { gui = "italic,bold" },
+					},
+					{
+						"progress",
+						color = { gui = "italic,bold" },
+					},
+				},
+				lualine_z = {
+					{
+						"datetime",
+						style = " %H:%M",
+						color = { gui = "italic,bold" },
+					},
+					{
+						codecompanion,
+					},
 				},
 			},
-			lualine_z = {
-				{
-					"datetime",
-					style = " %H:%M",
-					color = { gui = "italic,bold" },
-				},
-				{
-					require("plugins.codecompanion.utils.lualine"),
-				},
-			},
-		},
-	},
+		}
+	end,
 	dependencies = {
 		"letieu/harpoon-lualine",
 	},
